4223c884f8eeb571f416e1ee4f9dfa7c01c02d07
mahasultan <ajabor@andrew.cmu.edu>
2023-10-05 12:02:09 +0300

    Query 2

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,9 +8,11 @@ export default function QueryProcessor(query: string): string {
   } else if (query.toLowerCase().includes("andrew id")) {
     return "sastaneh";
   }
-  
+
  else if (query.toLowerCase().includes("What is your name?")) {
   return "Shahad + Al Maha";
+}   else if (query.toLowerCase().includes("which of the following numbers is the largest: 50, 75, 81?")) {
+  return "81";
 }
 
   return "";
